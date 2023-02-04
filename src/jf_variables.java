2e914b8d728fe788b4982700221760a0817ef664
jcbjsp <jacobjoseph9991@gmail.com>
2023-02-04 20:28:58 +0000

    jf_variables

---
+++ src/jf_variables.java
@@ -0,0 +1,12 @@
+public class jf_variables {
+    public static void main(String[] args) {
+        int  a; // declaration
+        a = 2345; // assignment
+        int b = 345; // declaration and assignment
+        double c = 123.456;
+        String name = "Britney";
+        System.out.println(name);
+        name = "Beyonce";
+        System.out.println(name);
+    }
+}
