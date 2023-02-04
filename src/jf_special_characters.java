c03bd5639a9b9d9b8cc5fe4ad9bd7a884aa55a26
jcbjsp <jacobjoseph9991@gmail.com>
2023-02-04 20:33:41 +0000

    jf_special_characters

---
+++ src/jf_special_characters.java
@@ -0,0 +1,10 @@
+public class jf_special_characters {
+    public static void main(String[] args) {
+        String firstName = "Britney";
+        String lastName = "Spears";
+        int age = 39;
+        // Britney's last name is "Spears"
+        System.out.println(firstName + "\'s last name is \"" + lastName + "\"");
+    }
+}
+
